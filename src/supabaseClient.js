48fb8f55ef409ad198285199c0c2e2629e17583c
Kangji-hye <joyloop7@gmail.com>
2026-01-11 11:42:12 +0900

    test:vercel 셋팅관련

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -5,12 +5,6 @@ import { createClient } from "@supabase/supabase-js";
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-// SUPABASE_ANON_KEY는 외부에 접근을 하면 안되는 키다. 
-
-// 디버깅용 (개발 중에만 잠깐 사용)
-console.log("SUPABASE_URL:", SUPABASE_URL);
-console.log("SUPABASE_ANON_KEY 앞부분:", SUPABASE_ANON_KEY?.slice(0, 2));
-
 // 클라이언트 생성
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
